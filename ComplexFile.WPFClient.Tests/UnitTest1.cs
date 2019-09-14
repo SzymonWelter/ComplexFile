8e2356a58ae862dc244e13fe0d10c38b75c7552e
SzymonWelter <szymon.welter98@gmail.com>
2019-09-14 13:47:24 +0200

    init

---
+++ ComplexFile.WPFClient.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ComplexFile.WPFClient.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
